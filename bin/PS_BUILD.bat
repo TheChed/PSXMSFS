9d5e1e8a6cb4114d8714bd480df86a9dba7576d2
The Ched <83767929+TheChed@users.noreply.github.com>
2022-04-20 18:38:27 +0800

    Binaries added in the Release link of Github

--- bin/PS_BUILD.bat
+++
@@ -1,4 +0,0 @@
-gcc -g -Iinclude -c connect.cpp 
-gcc -g -Iinclude -c PSX.cpp 
-gcc -g -Iinclude -c PSXMSFS.cpp 
-gcc -g -o PSXMSFS.exe PSXMSFS.o PSX.o connect.o -Lbin -lwsock32 -lSimConnect -lpthread 
