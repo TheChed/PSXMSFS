aa8bfc9b2533d1afdcc9d53e2bae304cdff8e8e2
stephan <stephan@theched.org>
2023-06-22 09:30:02 +0800

    2023-06-22 Optimizing files for Win10 native compilation

--- MSFS.cpp
+++ MSFS.cpp
@@ -242,6 +242,7 @@ void CALLBACK SimmConnectProcess(SIMCONNECT_RECV *pData, DWORD cbData, void *pCo
         } break;
 
         case EVENT_P_PRESS: {
+            printDebug(LL_ERROR, "In P Event:  %d\n",evt->uEventID);
             if (!key_press) {
                 key_press = 1;
                 SimConnect_TransmitClientEvent(
@@ -265,7 +266,7 @@ void CALLBACK SimmConnectProcess(SIMCONNECT_RECV *pData, DWORD cbData, void *pCo
         } break;
 
         default:
-            printDebug(LL_ERROR, "Event not captured");
+            printDebug(LL_ERROR, "Event %i not captured",evt->uEventID);
         }
     } break;
 
@@ -505,7 +506,6 @@ int init_MS_data(void)
                                              "FREEZE_LATITUDE_LONGITUDE_SET");
     hr = SimConnect_MapClientEventToSimEvent(hSimConnect, EVENT_FREEZE_LAT_LONG_TOGGLE,
                                              "FREEZE_LATITUDE_LONGITUDE_TOGGLE");
-
     /*
      * EVENT used to set the parking break
      */
