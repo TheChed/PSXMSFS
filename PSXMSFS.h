6c751e03f6b56ff36c33f7dbe3a033b6766ecaf5
Stephan <stephan@theched.org>
2023-06-29 17:09:02 +0800

    2023-06-28 Changin DLL entry point

--- PSXMSFS.h
+++ PSXMSFS.h
@@ -3,6 +3,4 @@
 #include <windows.h>
 
 extern "C" __declspec(dllexport) DWORD main_launch(int argc, char** argv);
-//extern "C" __declspec(dllexport) DWORD cleanup(void);
-//extern "C" __declspec(dllexport) DWORD initialize(int argc, char **argv);
 #endif
