12a8b2571104f7e629c76e447ab65fda234195da
Stephan <stephan@theched.org>
2023-08-03 18:28:04 +0800

    2023-08-03 compartimentalizing functions

---
+++ connect.h
@@ -0,0 +1,6 @@
+
+int init_param(const char *MSFSServerIP, const char *PSXMainIP, int PSXMainPort, const char *PSXBoostIP, int PSXBoostPort);
+int init_socket(void);
+int close_PSX_socket(SOCKET socket);
+int open_connections();
+int init_connect_MSFS(HANDLE *p);
